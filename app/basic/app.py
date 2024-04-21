974864d0abc7ca20ee16ee5bf6437d4962bb7b0b
miskowic <miskowicz@student..agh.edu.pl>
2024-04-22 01:05:00 +0200

    create 3 app versions (basic, premium and legacy) alongside docker images

---
+++ app/basic/app.py
@@ -0,0 +1,14 @@
+# basic/app.py
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return 'Hello from the basic version!'
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ app/legacy/app.py
@@ -0,0 +1,15 @@
+# legacy/app.py
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return 'Hello from the legacy version!'
+
+
+if __name__ == '__main__':
+    app.run()
+

192159994a02fd9dcd89308d103f44948916c931
steczkiewicz <pawelsteczkiewicz1@gmail.com>
2024-05-28 00:16:42 +0200

    remove comments

--- appjs/chat.js
+++ appjs/chat.js
@@ -1,15 +1,14 @@
 const dgram = require('dgram');
 const client = dgram.createSocket('udp4');
 
-const PORT = 12345; // Hardcoded port number
-const HOST = 'localhost'; // Target host
+const PORT = 12345;
+const HOST = 'localhost';
 
 const messages = ['hello', 'hi', 'whatsup'];
 
 setInterval(() => {
-    // Randomly select a message
     const message = Buffer.from(messages[Math.floor(Math.random() * messages.length)]);
     client.send(message, 0, message.length, PORT, HOST, () => {
         console.log(`Message sent to ${HOST}:${PORT}: ${message}`);
     });
-}, 3000); // 3000 milliseconds = 3 seconds
+}, 3000);
